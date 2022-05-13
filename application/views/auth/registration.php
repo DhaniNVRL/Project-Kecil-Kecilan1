ad5e5a51f9458fddf372717c0da6ec6ce454cee8
Firdhani N <68591251+DhaniNVRL@users.noreply.github.com>
2022-05-13 11:01:50 +0700

    Update registration.php

--- application/views/auth/registration.php
+++ application/views/auth/registration.php
@@ -1,53 +1,52 @@
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-7">
+<div class="container">
 
-                <div class="card o-hidden border-0 shadow-lg my-5">
-                    <div class="card-body p-0">
-                        <!-- Nested Row within Card Body -->
-                        <div class="row">
-                            <div class="col-lg">
-                                <div class="p-5">
-                                    <div class="text-center">
-                                        <h1 class="h4 text-gray-900 mb-4">Create an Account!</h1>
+    <!-- Outer Row -->
+    <div class="row justify-content-center">
+
+        <div class="col-7">
+
+            <div class="card o-hidden border-0 shadow-lg my-5">
+                <div class="card-body p-0">
+                    <!-- Nested Row within Card Body -->
+                    <div class="row">
+                        <div class="col">
+                            <div class="p-5">
+                                <div class="text-center">
+                                    <h1 class="h4 text-gray-900 mb-4">Login</h1>
+                                </div>
+
+                                <!-- Alert Akun Sukses Terdaftar -->
+                                <?php $pesan = $this->session->flashdata('pesan');
+                                if (isset($pesan)) {
+                                    echo '<div class="alert alert-success" role="alert">
+                                    Akun Sukses di Daftarkan </div>';
+                                } ?>
+
+                                <!-- Form Login -->
+                                <form class="user">
+                                    <div class="form-group">
+                                        <input type="text" class="form-control form-control-user" id="email" placeholder="Enter Email Address..." name="emial">
                                     </div>
-                                    <form class="user" method="POST" action="<?php echo base_url('auth/registration') ?>">
-                                        <div class="form-group row">
-                                            <div class="col-sm-6 mb-3 mb-sm-0">
-                                                <input type="text" class="form-control form-control-user" id="first_name" placeholder="First Name" name="first_name">
-                                            </div>
-                                            <div class="col-sm-6">
-                                                <input type="text" class="form-control form-control-user" id="last_name" placeholder="Last Name" name="last_name">
-                                            </div>
-                                        </div>
-                                        <div class=" form-group">
-                                            <input type="email" class="form-control form-control-user" id="exampleInputEmail" placeholder="Email Address">
-                                        </div>
-                                        <div class="form-group row">
-                                            <div class="col-sm-6 mb-3 mb-sm-0">
-                                                <input type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Password">
-                                            </div>
-                                            <div class="col-sm-6">
-                                                <input type="password" class="form-control form-control-user" id="exampleRepeatPassword" placeholder="Repeat Password">
-                                            </div>
-                                        </div>
-                                        <a href="login.html" class="btn btn-primary btn-user btn-block">
-                                            Register Account
-                                        </a>
-                                    </form>
-                                    <hr>
-                                    <div class="text-center">
-                                        <a class="small" href="<?php echo base_url('auth/forgot') ?>">Forgot Password?</a>
+                                    <div class="form-group">
+                                        <input type="password" class="form-control form-control-user" id="password" placeholder="Password" name="password">
                                     </div>
-                                    <div class="text-center">
-                                        <a class="small" href="<?php echo base_url('auth') ?>">Already have an account? Login!</a>
-                                    </div>
+                                    <button type="submit" class="btn btn-primary btn-user btn-block">
+                                        Login
+                                    </button>
+                                </form>
+                                <hr>
+                                <div class="text-center">
+                                    <a class="small" href="<?php echo base_url('auth/forgot') ?>">Forgot Password?</a>
+                                </div>
+                                <div class="text-center">
+                                    <a class="small" href="<?php echo base_url('auth/registration') ?>">Create an Account!</a>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
+
         </div>
 
     </div>
